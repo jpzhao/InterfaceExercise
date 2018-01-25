bcabe580d628977142cc990db054adbbd41036ab
zhaojinping <zhaojinping@chupinxiu.com>
2018-01-26 02:10:36 +0800

    update

--- guest/sign/admin.py
+++ guest/sign/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from sign.models import Event,Guest
 
 # Register your models here.
+
+
+
+class EventAdmin(admin.ModelAdmin):
+	list_display = ['name','status','start_time','id']
+	search_fields = ['name']  #搜索栏
+	list_filter=['status']   #过滤器
+
+class GuestAdmin(admin.ModelAdmin):
+	list_display = ['realname','phone','email','sign','create_time','event']
+	list_display_links = ('realname','phone') #显示链接
+	search_fields = ['realname','phone']   #搜索栏
+	list_filter = ['event']                 #过滤器
+
+admin.site.register(Event,EventAdmin)
+admin.site.register(Guest,GuestAdmin)
+
