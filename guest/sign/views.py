71093e2e90c510806a13e47eb57661c1bf259866
zhaojinping <zhaojinping@chupinxiu.com>
2018-01-31 01:27:05 +0800

    update

--- guest/sign/views.py
+++ guest/sign/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse,HttpResponseRedirect
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
@@ -39,7 +39,14 @@ def event_manage(request):
     username=request.session.get('user','')
     return render(request,"event_manage.html",{"user":username,"events":event_list})
 
-@login_required()  #嘉宾管理
+#签到页面
+@login_required()
+def sign_index(request,event_id):
+    event=get_object_or_404(Event,id=event_id)
+    return render(request,'sign_index.html',{'event':event})
+
+#嘉宾管理
+@login_required()
 def guest_manage(request):
     username=request.session.get('user','')
     guest_list=Guest.objects.all()
@@ -60,4 +67,32 @@ def search_name(request):
     event_list=Event.objects.filter(name__contains=search_name)
     return render(request,"event_manage.html",{"user":username,"events":event_list})
 
+#签到动作（视图函数）
+@login_required()
+def sign_index_action(request,event_id):
+    event=get_object_or_404(Event,id=event_id)
+    phone=request.POST.get('phone','')
+
+    result=Guest.objects.filter(phone=phone)
+    if not result:
+        return render(request,'sign_index.html',{'event':event,'hint':'phone error.'})
+
+    result=Guest.objects.filter(phone=phone,event_id=event_id)
+    if not result:
+        return render(request,'sign_index.html',{'event':event,'hint':'event id or phone error.'})
+
+    result=Guest.objects.get(phone=phone,event_id=event_id)
+    if not result:
+        return render(request,'sign_index.html',{'event':event,'hint':'user has sign in.'})
+
+    else:
+        Guest.objects.filter(phone=phone,event_id=event_id).update(sign='1')
+        return render(request,'sign_index.html',{'event':event,'hint':'sign in success!','guest':result})
+
+#退出登录
+@login_required()
+def logout(request):
+    auth.logout(request)
+    response=HttpResponseRedirect('/index/')
+    return response
 
