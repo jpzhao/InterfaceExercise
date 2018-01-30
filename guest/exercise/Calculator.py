359d089f8064e8208288201ea42b4f5329457711
zhaojinping <zhaojinping@chupinxiu.com>
2018-01-31 02:00:36 +0800

    update

---
+++ guest/exercise/Calculator.py
@@ -0,0 +1,28 @@
+'''
+Author:chongshi
+Date:2018/01/31
+Describe:加减乘除
+'''
+
+class Calculator():
+	def __init__(self,a,b):
+		self.a=int(a)
+		self.b=int(b)
+
+	def add(self):
+		return self.a+self.b
+
+
+	def sub(self):
+		return self.a-self.b
+
+
+	def mul(self):
+		return self.a * self.b
+
+
+	def div(self):
+		return self.a/self.b
+
+test=Calculator(5,3)
+print(test)
