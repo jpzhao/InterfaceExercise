f796e1ba54d88e510d99e4430ba5c3a85e4aec62
zhaojinping <zhaojinping@chupinxiu.com>
2018-04-18 07:17:22 +0800

    numstr

--- WrapMe.py
+++ WrapMe.py
@@ -6,7 +6,7 @@ class WrapMe(object):
         return self.__data
 
     def __repr__(self):
-        return 'self.__data'
+        return repr('self.__data')
 
     def __str__(self):
         return str(self.__data)
@@ -14,5 +14,12 @@ class WrapMe(object):
     def __getattr__(self, attr):
         return getattr(self.__data,attr)
 
-wrappedComplex=WrapMe(3.5+4.2j)
-print (wrappedComplex)
+# wrappedComplex=WrapMe(3.5+4.2j)
+# print (wrappedComplex)
+# print (wrappedComplex.imag)
+# print (wrappedComplex.real)
+# print (wrappedComplex.conjugate())
+# print (wrappedComplex.get())
+
+wrappedList=WrapMe([123,'foo',45.67])
+print(wrappedList.append('bar'))
