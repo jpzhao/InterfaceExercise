a2f847112dbbd82ca5d97e2e5d036dd2d40c2289
zhaojinping <zhaojinping@chupinxiu.com>
2018-04-17 10:03:31 +0800

    update

--- randSeq.py
+++ randSeq.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 from random import choice
-
-class RandSeq(object):
+class randSeq(object):
     def __init__(self,seq):
         self.data=seq
 
     def __iter__(self):
         return self
 
-    def next(self):
+    def __next__(self):      #python2+:next(), python3+:__next()__
         return choice(self.data)
+
+tst=randSeq(['abc','ab'])
+for i in tst:
+    print(i)
